15988fe75e0417814ebe5c9d0f8d770f312ee391
Joshua Eddy <joshiceddy@hotmail.com>
2019-10-16 17:59:58 +0100

    Addded functions [does not compile]

--- Revoked.Web/Client/src/Revoked/Helper.purs
+++ Revoked.Web/Client/src/Revoked/Helper.purs
@@ -6,9 +6,13 @@ import Class.Object (class Object, position, size)
 import Collision (isCollMap, isCollWorld)
 import Constants (mapSize)
 import Data.Player (Player(..))
+import Data.Array (range, zipWith, filter, head)
+import Data.Maybe (Maybe(..))
+import Data.Int (toNumber, floor)
 import Emo8.Action.Draw (Draw, emap)
 import Emo8.Action.Update (Update)
-import Emo8.Types (MapId, X)
+import Emo8.Types (MapId, X, Width, Height)
+import Types (Pos)
 import Emo8.Utils (defaultMonitorSize)
 
 beInMonitor :: Player -> Player -> Player
@@ -50,6 +54,29 @@ drawScrollMap distance = do
             where
                 base = num * mapWidth
 
+mapCollisionPostition :: forall a. Object a => X -> a -> a -> Maybe Pos
+mapCollisionPostition distance oldState newState = head collidedPositions
+    where 
+        vector = { 
+            deltaX: (position newState).x - (position oldState).x, 
+            deltaY: (position newState).y - (position oldState).y 
+        }
+        numberOfSteps = if vector.deltaX > vector.deltaY then vector.deltaX else vector.deltaY
+        increment = { 
+            x: (toNumber vector.deltaX) / (toNumber numberOfSteps), 
+            y: (toNumber vector.deltaY) / (toNumber numberOfSteps)
+        }
+        steps = range 0 numberOfSteps
+        values = { 
+            x: map (\x -> (toNumber (position oldState).x) + ((toNumber x) * increment.x)) steps, 
+            y: map (\y -> (toNumber (position oldState).y) + ((toNumber y) * increment.y)) steps
+        }
+        positionsAlongVector = zipWith (\x y -> { x: x, y: y }) values.x values.y
+        collidedPositions = filter (\position -> do 
+            result <- isCollideScrollMapN distance position (size oldState) (size oldState)
+            result
+        )
+
 -- TODO: readable
 isCollideScrollMap :: forall a. Object a => X -> a -> Update Boolean
 isCollideScrollMap distance o =
@@ -63,6 +90,22 @@ isCollideScrollMap distance o =
         collCond mId num d = do
             if (base - mapSize * mapTileInMonitor <= d && d < base + mapWidth)
                 then isCollMap mId mapSize (size o) { x: (position o).x + (d - base), y: (position o).y }
+                else pure false
+            where
+                base = num * mapWidth
+
+isCollideScrollMapN :: X -> Pos -> Width -> Height -> Update Boolean
+isCollideScrollMapN distance position width height =
+    (\a b c d -> a || b || c || d)
+        <$> collCond 0 0 distance
+        <*> collCond 1 1 distance
+        <*> collCond 2 2 distance
+        <*> collCond 3 3 distance
+    where
+        collCond :: MapId -> Int -> X -> Update Boolean
+        collCond mId num d = do
+            if (base - mapSize * mapTileInMonitor <= d && d < base + mapWidth)
+                then isCollMap mId mapSize width { x: position.x + (d - base), y: position.y }
                 else pure false
             where
                 base = num * mapWidth
