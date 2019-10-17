bb029ad1357f57994d905d25d7ad370b68770e2d
Joshua Eddy <joshiceddy@hotmail.com>
2019-10-17 11:04:44 +0100

    Added scroll back to level

--- Revoked.Web/Client/src/Revoked/Data/Sprites.purs
+++ Revoked.Web/Client/src/Revoked/Data/Sprites.purs
@@ -1,6 +1,5 @@
 module Data.Sprites where
 
-import Prelude
 import Emo8.Types (Sprite)
 import Assets.Sprites.Player.Sprite as RawPlayer
 
--- Revoked.Web/Client/src/Revoked/Revoked.purs
+++ Revoked.Web/Client/src/Revoked/Revoked.purs
@@ -86,7 +86,7 @@ instance gameState :: Game State where
             true, _ -> ClearState
             false, true -> OverState
             false, false -> PlayState $ s { 
-                distance = s.distance, 
+                distance = s.distance + speed, 
                 player = nnp, 
                 bullets = nnbullets <> newBullets, 
                 enemies = nnenemies <> newEnemies, 
