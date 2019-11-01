baac9a763889517f50291db05f3659741c02e252
Joshua Eddy <joshiceddy@hotmail.com>
2019-11-01 12:42:11 +0000

    Fixed variable names

--- Revoked.Web/Client/test/Test/Revoked/Data/Player/AdjustX.purs
+++ Revoked.Web/Client/test/Test/Revoked/Data/Player/AdjustX.purs
@@ -14,81 +14,81 @@ adjustXTests =
         -- Moving Left
         test "shouldAdjustXCorrectlyWhenMovingLeft [12, 64, 0 -> 20]" do
             let 
-                oldY = 64
-                newY = 0
+                oldX = 64
+                newX = 0
                 distance = 12
 
                 expectedResult = 20
-                result = adjustX oldY newY distance
+                result = adjustX oldX newX distance
             equal expectedResult result
 
         test "shouldAdjustXCorrectlyWhenMovingLeft [0, 64, 0 -> 32]" do
             let 
-                oldY = 64
-                newY = 0
+                oldX = 64
+                newX = 0
                 distance = 0
 
                 expectedResult = 32
-                result = adjustX oldY newY distance
+                result = adjustX oldX newX distance
             equal expectedResult result
 
         test "shouldAdjustXCorrectlyWhenMovingLeft [0, 32, 30 -> 32]" do
             let 
-                oldY = 32
-                newY = 30
+                oldX = 32
+                newX = 30
                 distance = 0
 
                 expectedResult = 32
-                result = adjustX oldY newY distance
+                result = adjustX oldX newX distance
             equal expectedResult result
 
         test "shouldAdjustXCorrectlyWhenMovingLeft [0, 7, 4 -> 32]" do
             let 
-                oldY = 7
-                newY = 4
+                oldX = 7
+                newX = 4
                 distance = 0
 
                 expectedResult = 32
-                result = adjustX oldY newY distance
+                result = adjustX oldX newX distance
             equal expectedResult result
 
         -- Moving Right
         test "shouldAdjustXCorrectlyWhenMovingRight [12, 0, 64 -> 52]" do
             let 
-                oldY = 0
-                newY = 64
+                oldX = 0
+                newX = 64
                 distance = 12
 
                 expectedResult = 52
-                result = adjustX oldY newY distance
+                result = adjustX oldX newX distance
             equal expectedResult result
 
         test "shouldAdjustXCorrectlyWhenMovingRight [0, 0, 64 -> 64]" do
             let 
-                oldY = 0
-                newY = 64
+                oldX = 0
+                newX = 64
                 distance = 0
 
                 expectedResult = 64
-                result = adjustX oldY newY distance
+                result = adjustX oldX newX distance
             equal expectedResult result
 
         test "shouldAdjustXCorrectlyWhenMovingRight [0, 30, 32 -> 32]" do
             let 
-                oldY = 30
-                newY = 32
+                oldX = 30
+                newX = 32
                 distance = 0
 
                 expectedResult = 32
-                result = adjustX oldY newY distance
+                result = adjustX oldX newX distance
             equal expectedResult result
 
         test "shouldAdjustXCorrectlyWhenMovingRight [0, 4, 7 -> 0]" do
             let 
-                oldY = 4
-                newY = 7
+                oldX = 4
+                newX = 7
                 distance = 0
 
                 expectedResult = 0
-                result = adjustX oldY newY distance
+                result = adjustX oldX newX distance
             equal expectedResult result
