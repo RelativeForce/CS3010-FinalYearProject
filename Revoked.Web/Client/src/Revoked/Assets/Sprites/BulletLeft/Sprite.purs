9b3cd32d1b9926c5a036223ba987b124d406ed8d
Joshua Eddy <joshiceddy@hotmail.com>
2019-11-06 16:35:25 +0000

    Added bullet left images

--- Revoked.Web/Client/src/Revoked/Assets/Sprites/BulletLeft/Sprite.purs
+++ Revoked.Web/Client/src/Revoked/Assets/Sprites/BulletLeft/Sprite.purs
@@ -2,15 +2,16 @@ module Assets.Sprites.BulletLeft.Sprite where
 
 import Emo8.Types (Sprite)
 import Assets.Sprites.BulletLeft.Frame0 (frame0Data)
+import Assets.Sprites.BulletLeft.Frame1 (frame1Data)
 import Assets.AssetIds as Id
 
 bulletLeft ::  Sprite
 bulletLeft = {
-    frames: [frame0Data],
+    frames: [frame0Data, frame1Data],
     frameIndex: 0,
-    framesPerSecond: 1,
-    frameCount: 1,
-    width: 32,
-    height: 32,
+    framesPerSecond: 2,
+    frameCount: 2,
+    width: 18,
+    height: 10,
     id: Id.bulletSpriteBackwardId
 }
