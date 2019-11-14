afe11b51cbfac593523563cb9e1bfe190e0599fc
Joshua Eddy <joshiceddy@hotmail.com>
2019-11-14 16:14:14 +0000

    Removed old tests

--- Revoked.Web/Client/test/Test/Engine/Emo8/Parse/Parse.purs
+++ Revoked.Web/Client/test/Test/Engine/Emo8/Parse/Parse.purs
@@ -6,17 +6,13 @@ import Prelude
 
 import Effect (Effect)
 import Test.Unit.Main (runTest)
-import Test.Engine.Emo8.Parse.ParseSound (parseSoundTests)
 import Test.Engine.Emo8.Parse.MapSum (mapSumTests)
-import Test.Engine.Emo8.Parse.SoundSum (soundSumTests)
 
 parseTests :: Effect Unit
 parseTests = do
     -- Tests
     runTest do
-        parseSoundTests
         mapSumTests
-        soundSumTests
     -- Sub Modules
 
     
