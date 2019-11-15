a7db09a04d909b52d27a07a49416e1e2c6a47ecb
Joshua Eddy <joshiceddy@hotmail.com>
2019-11-15 16:30:39 +0000

    Added all alphabet characters as inputs

--- Revoked.Web/Client/src/Engine/Emo8/Data/KeyTouchInput.purs
+++ Revoked.Web/Client/src/Engine/Emo8/Data/KeyTouchInput.purs
@@ -1,7 +1,7 @@
-module Emo8.Data.KeyTouchInput
-  ( KeyTouchInput(..)
-  , anyKeyTouch
-  ) where
+module Emo8.Data.KeyTouchInput ( 
+  KeyTouchInput(..), 
+  anyKeyTouch
+) where
 
 import Prelude
 
@@ -14,12 +14,34 @@ newtype KeyTouchInput = KeyTouchInput {
   isRight :: Boolean, 
   isUp :: Boolean, 
   isDown :: Boolean, 
-  isW :: Boolean, 
-  isA :: Boolean, 
-  isS :: Boolean, 
-  isD :: Boolean,
   isSpace :: Boolean,
-  isEnter :: Boolean
+  isEnter :: Boolean,
+  isA :: Boolean,
+  isB :: Boolean,  
+  isC :: Boolean, 
+  isD :: Boolean,
+  isE :: Boolean, 
+  isF :: Boolean, 
+  isG :: Boolean, 
+  isH :: Boolean, 
+  isI :: Boolean, 
+  isJ :: Boolean, 
+  isK :: Boolean, 
+  isL :: Boolean, 
+  isM :: Boolean, 
+  isN :: Boolean, 
+  isO :: Boolean, 
+  isP :: Boolean, 
+  isQ :: Boolean, 
+  isR :: Boolean, 
+  isS :: Boolean,
+  isT :: Boolean, 
+  isU :: Boolean,  
+  isV :: Boolean, 
+  isW :: Boolean,
+  isX :: Boolean, 
+  isY :: Boolean, 
+  isZ :: Boolean
 }
 
 instance inputKeyTouchInput :: Input KeyTouchInput where
@@ -34,16 +56,67 @@ mergeInput (KeyInput k) (TouchInput t) = KeyTouchInput {
   isRight: k.isRight || t.isRight, 
   isUp: k.isUp || t.isUp, 
   isDown: k.isDown || t.isDown, 
-  isW: k.isW || t.isW, 
-  isA: k.isA || t.isA, 
-  isS: k.isS || t.isS, 
-  isD: k.isD || t.isD,
   isSpace: k.isSpace,
-  isEnter: k.isEnter
+  isEnter: k.isEnter,
+  isA: k.isA,
+  isB: k.isB,  
+  isC: k.isC, 
+  isD: k.isD,
+  isE: k.isE, 
+  isF: k.isF, 
+  isG: k.isG, 
+  isH: k.isH, 
+  isI: k.isI, 
+  isJ: k.isJ, 
+  isK: k.isK, 
+  isL: k.isL, 
+  isM: k.isM, 
+  isN: k.isN, 
+  isO: k.isO, 
+  isP: k.isP, 
+  isQ: k.isQ, 
+  isR: k.isR, 
+  isS: k.isS,
+  isT: k.isT, 
+  isU: k.isU,  
+  isV: k.isV, 
+  isW: k.isW,
+  isX: k.isX, 
+  isY: k.isY, 
+  isZ: k.isZ
 }
 
 anyKeyTouch :: KeyTouchInput -> Boolean
-anyKeyTouch (KeyTouchInput i)
-  = i.isW || i.isS || i.isA || i.isD
-  || i.isUp || i.isDown || i.isLeft || i.isRight 
-  || i.isSpace || i.isEnter
+anyKeyTouch (KeyTouchInput i) = 
+  i.isUp 
+  || i.isDown 
+  || i.isLeft 
+  || i.isRight 
+  || i.isSpace 
+  || i.isEnter
+  || i.isA
+  || i.isB  
+  || i.isC 
+  || i.isD
+  || i.isE 
+  || i.isF 
+  || i.isG 
+  || i.isH 
+  || i.isI 
+  || i.isJ 
+  || i.isK 
+  || i.isL 
+  || i.isM 
+  || i.isN 
+  || i.isO 
+  || i.isP 
+  || i.isQ 
+  || i.isR 
+  || i.isS
+  || i.isT 
+  || i.isU  
+  || i.isV 
+  || i.isW
+  || i.isX 
+  || i.isY 
+  || i.isZ
