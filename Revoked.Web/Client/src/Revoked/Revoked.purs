fac52a8d6cb6dd3dedb6e15d6ce41114158b10a4
RelativeForce <joshiceddy@hotmail.com>
2019-12-08 12:52:45 +0000

    Refactored Leaderboard update into seperate module

--- Revoked.Web/Client/src/Revoked/Revoked.purs
+++ Revoked.Web/Client/src/Revoked/Revoked.purs
@@ -2,50 +2,34 @@ module Revoked where
 
 import Prelude
 
-import Assets.Audio as A
 import Assets.Images as I
-import Class.Object (draw, position, scroll)
-import Collision (isCollideObjects, isOutOfWorld)
-import Constants (scoreDisplayX, scoreDisplayY, scoreDisplayTextHeight, maxUsernameLength)
-import Data.Array (any, filter, partition, length, init)
-import Data.Bullet (Bullet, updateBullet)
-import Data.DateTime (DateTime)
+import Class.Object (draw)
+import Constants (scoreDisplayX, scoreDisplayY, scoreDisplayTextHeight)
 import Data.Either (Either(..))
-import Data.Enemy (Enemy, addEnemyBullet, updateEnemy, enemyToScore)
-import Data.EnemyBullet (EnemyBullet, updateEnemyBullet)
-import Data.Foldable (traverse_, sum)
-import Data.Goal (Goal, updateGoal)
-import Data.Maybe (Maybe(..))
-import Data.Particle (Particle, initParticle, updateParticle)
-import Data.Player (Player, addBullet, initialPlayer, updatePlayer)
-import Data.String (joinWith)
+import Data.Foldable (traverse_)
 import Effect (Effect)
 import Emo8 (emo8)
 import Emo8.Action.Draw (drawScaledImage, drawText)
-import Emo8.Action.Update (nowDateTime, listTopScores, storePlayerScore, isAudioStreamPlaying, stopAudioStream, addAudioStream)
+import Emo8.Action.Update (nowDateTime)
 import Emo8.Class.Game (class Game)
 import Emo8.Data.Color (Color(..))
-import Emo8.FFI.AudioController (AudioController, newAudioController)
-import Emo8.Input (isCatchAny, mapToCharacter)
-import Emo8.Types (MapId, Score, PlayerScore)
+import Emo8.Input (isCatchAny)
 import Emo8.Utils (defaultMonitorSize, mkAsset)
-import Helper (drawScrollMap, isCollideMapWalls, isCollideMapHazards, adjustMonitorDistance, drawUsername, formatDateTime, drawScore)
-import Levels (allRawLevels, enemies, goals, levelCount)
+import Helper (drawScrollMap, drawUsername, drawScore)
+import Levels (allRawLevels)
 import States.Play (PlayState, updatePlay, initialPlayState)
 import States.Victory (VictoryState, updateVictory, initialVictoryState)
+import States.Leaderboard (LeaderboardState, updateLeaderboard, initialLeaderboardState)
 
 data State = 
     TitleScreen 
     | GameOver
-    | Leaderboard {
-        scores :: Array PlayerScore,
-        isWaiting :: Boolean,
-        isLoaded :: Boolean
-    }
+    | Leaderboard LeaderboardState
     | Victory VictoryState
     | Play PlayState
 
 instance gameState :: Game State where
+    -- update TitleScreen state
     update _ input TitleScreen = do
         startDateTime <- nowDateTime
 
@@ -54,39 +38,26 @@ instance gameState :: Game State where
             toPlay = isCatchAny input
 
         pure $ case toPlay, toLeaderboard of 
-            false, true -> initialLeaderboardState
+            false, true -> Leaderboard initialLeaderboardState
             true, false -> Play $ initialPlayState startDateTime
             _, _ -> TitleScreen
+    
+    -- update GameOver state
     update _ input GameOver =
         pure $ if isCatchAny input then TitleScreen else GameOver
+    
+    -- update Leaderboard state
     update _ input (Leaderboard s) = do
-        let 
-            hasNoScores = 0 == length s.scores
-            shouldLoadScores = not s.isLoaded && (s.isWaiting || hasNoScores)  
 
-        result <- if shouldLoadScores
-            then do listTopScores
-            else pure $ Left "AllowInput"
+        leaderboardStateOrNextStateId <- updateLeaderboard input s
 
-        let
-            backToTitleScreen = input.catched.isBackspace
-            isWaiting = case result of
-                Left "Waiting" -> true
-                _-> false
-            isLoaded = case result of
-                Right response -> true
-                _-> false
-            scores = case result of
-                Right response -> response
-                _-> s.scores
-            
-        pure $ case backToTitleScreen of
-            true -> TitleScreen
-            false -> Leaderboard $ s {
-                scores = scores,
-                isWaiting = isWaiting,
-                isLoaded = isLoaded
-            }
+        pure $ case leaderboardStateOrNextStateId of
+            Left leaderboard -> Leaderboard leaderboard
+            Right stateId -> case stateId of
+                1 -> TitleScreen
+                _ -> Leaderboard s
+
+    -- update Victory state
     update _ input (Victory s) = do
        
         victoryStateOrNextStateId <- updateVictory input s
@@ -97,6 +68,7 @@ instance gameState :: Game State where
                 1 -> TitleScreen
                 _ -> Victory s
 
+    -- update Play state
     update asset input (Play s) = do
     
         playStateOrNextStateId <- updatePlay asset input s
@@ -132,30 +104,6 @@ instance gameState :: Game State where
         traverse_ draw s.enemyBullets
         traverse_ draw s.goals
         drawText ("Score: " <> show s.score) scoreDisplayTextHeight scoreDisplayX scoreDisplayY Lime
-
-newLevel :: MapId -> Score -> AudioController -> DateTime -> State
-newLevel mapId score audioController start = Play { 
-    distance: 0, 
-    player: initialPlayer, 
-    bullets: [], 
-    enemies: enemies mapId, 
-    particles: [], 
-    enemyBullets : [],
-    goals: goals mapId,
-    mapId: mapId,
-    score: score,
-    audioController: audioController,
-    start: start
-}
-
- 
-
-initialLeaderboardState :: State
-initialLeaderboardState = Leaderboard {
-    scores: [],
-    isWaiting: false,
-    isLoaded: false
-}
 
 initialState :: State
 initialState = TitleScreen
