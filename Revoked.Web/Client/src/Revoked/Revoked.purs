b7155146989c1bd93047c53c80ab4fcf423afd51
RelativeForce <joshiceddy@hotmail.com>
2019-12-08 11:57:57 +0000

    Pulled Play state into seperate module

--- Revoked.Web/Client/src/Revoked/Revoked.purs
+++ Revoked.Web/Client/src/Revoked/Revoked.purs
@@ -39,6 +39,7 @@ import Helper (
     drawScore
 )
 import Levels (allRawLevels, enemies, goals, levelCount)
+import State.Play (PlayState, updatePlay, initialPlayState)
 
 data State = 
     TitleScreen 
@@ -56,19 +57,7 @@ data State =
         end :: DateTime,
         isWaiting :: Boolean
     }
-    | Play { 
-        distance :: Int, 
-        player :: Player, 
-        bullets :: Array Bullet, 
-        enemies :: Array Enemy,
-        particles :: Array Particle, 
-        enemyBullets :: Array EnemyBullet,
-        goals :: Array Goal,
-        mapId :: MapId,
-        score :: Score,
-        audioController :: AudioController,
-        start :: DateTime
-    }
+    | Play PlayState
 
 instance gameState :: Game State where
     update _ input TitleScreen = do
@@ -80,7 +69,7 @@ instance gameState :: Game State where
 
         pure $ case toPlay, toLeaderboard of 
             false, true -> initialLeaderboardState
-            true, false -> initialPlayState startDateTime
+            true, false -> Play $ initialPlayState startDateTime
             _, _ -> TitleScreen
     update _ input GameOver =
         pure $ if isCatchAny input then TitleScreen else GameOver
@@ -155,83 +144,16 @@ instance gameState :: Game State where
                 isWaiting = isWaiting
             }
     update asset input (Play s) = do
-
-        -- adjust player, entities and map for scrolling
-        let updatedPlayer = updatePlayer input s.player s.distance (isCollideMapWalls asset s.mapId s.distance)
-            newDistance = adjustMonitorDistance updatedPlayer s.distance
-            scrollOffset = (s.distance - newDistance)
-            scrollAdjustedPlayer = scroll scrollOffset updatedPlayer
-            scrollAdjustedBullets = map (scroll scrollOffset) s.bullets
-            scrollAdjustedEnemies = map (scroll scrollOffset) s.enemies
-            scrollAdjustedGoals = map (scroll scrollOffset) s.goals
-            scrollAdjustedParticles = map (scroll scrollOffset) s.particles
-            scrollAdjustedEnemyBullets = map (scroll scrollOffset) s.enemyBullets
-
-        -- updated entities
-        let { yes: enemiesInView, no: enemiesNotInView } = partition (not <<< isOutOfWorld) scrollAdjustedEnemies
-            updatedBullets = map updateBullet scrollAdjustedBullets
-            updatedEnemies = map (updateEnemy (isCollideMapWalls asset s.mapId s.distance) s.distance s.player) enemiesInView
-            updatedGoals = map updateGoal scrollAdjustedGoals
-            updatedParticles = map updateParticle scrollAdjustedParticles
-            updatedEnemyBullets = map updateEnemyBullet scrollAdjustedEnemyBullets
-
-        -- player collision
-        let hasCollidedHazard = isCollideMapHazards asset s.mapId s.distance scrollAdjustedPlayer
-            hasCollidedEnemy = any (isCollideObjects scrollAdjustedPlayer) updatedEnemies
-            hasCollidedEnemyBullet = any (isCollideObjects scrollAdjustedPlayer) updatedEnemyBullets
-            hasCollidedGoal = any (isCollideObjects scrollAdjustedPlayer) s.goals
-
-        -- collision with enemies
-        let { yes: collidedEnemies, no: notCollidedEnemies } = partition (\e -> any (isCollideObjects e) updatedBullets) updatedEnemies
-            { yes: collidedBullets, no: notCollidedBullets } = partition (\b -> any (isCollideObjects b) updatedEnemies) updatedBullets
-
-        -- add new entities
-        let newBullets = addBullet input s.player
-            newParticles = map (\e -> initParticle (position e)) collidedEnemies
-            newEnemyBullets = notCollidedEnemies >>= addEnemyBullet s.player
-            newScore = sum $ map enemyToScore collidedEnemies
-
-        -- delete entities (out of monitor)
-        let updatedBulletsInView = filter (not <<< isOutOfWorld) notCollidedBullets
-            updatedEnemyBulletsInView = filter (not <<< isOutOfWorld) updatedEnemyBullets
-            updatedParticlesInView = filter (not <<< isOutOfWorld) updatedParticles
-
-		-- delete entities (map collision)
-        let notCollidedWithMapBullets = filter (not <<< (isCollideMapWalls asset s.mapId s.distance)) updatedBulletsInView
-            notCollidedWithMapEnemyBullets = filter (not <<< (isCollideMapWalls asset s.mapId s.distance)) updatedEnemyBulletsInView
-
-        -- evaluate game condition
-        let isGameOver = hasCollidedHazard || hasCollidedEnemy || hasCollidedEnemyBullet
-            isNextLevel = hasCollidedGoal
-            isLastLevel = s.mapId + 1 >= levelCount
-
-        isBackgroundMusicPlaying <- isAudioStreamPlaying s.audioController A.backgroundMusicId
-
-        -- update music
+    
+        playStateOrNextStateId <- updatePlay asset input s
         now <- nowDateTime
-        newAudioController <- case isBackgroundMusicPlaying, (isGameOver || (isNextLevel && isLastLevel)) of
-                true, true -> stopAudioStream s.audioController A.backgroundMusicId
-                false, false ->  addAudioStream s.audioController A.backgroundMusicId
-                _, _ -> pure s.audioController
-        
-        pure $ case isGameOver, isNextLevel of
-            true, _ -> GameOver
-            false, true -> if isLastLevel 
-                then initialVictoryState s.score s.start now 
-                else newLevel (s.mapId + 1) s.score s.audioController s.start
-            false, false -> Play $ s { 
-                distance = newDistance, 
-                player = scrollAdjustedPlayer, 
-                bullets = notCollidedWithMapBullets <> newBullets, 
-                enemies = notCollidedEnemies <> enemiesNotInView, 
-                particles = updatedParticlesInView <> newParticles, 
-                enemyBullets = notCollidedWithMapEnemyBullets <> newEnemyBullets,
-                goals = updatedGoals,
-                mapId = s.mapId,
-                score = s.score + newScore,
-                audioController = newAudioController,
-                start = s.start
-            }
+
+        pure $ case playStateOrNextStateId of
+            Left play -> Play play
+            Right stateId -> case stateId of
+                2 -> GameOver
+                4 -> initialVictoryState s.score s.start now
+                _ -> Play s
 
     draw TitleScreen = do
         drawScaledImage I.titleScreen 0 0
@@ -271,9 +193,6 @@ newLevel mapId score audioController start = Play {
     audioController: audioController,
     start: start
 }
-
-initialPlayState :: DateTime -> State
-initialPlayState = newLevel 0 0 $ newAudioController "Play"
 
 initialVictoryState :: Score -> DateTime -> DateTime -> State
 initialVictoryState score start end = Victory {
---
+++ Revoked.Web/Client/src/Revoked/State/StateIds.purs
@@ -0,0 +1,18 @@
+module State.StateIds where
+
+import Emo8.Types (StateId)
+
+titleScreenId :: StateId
+titleScreenId = 1
+
+gameOverId :: StateId
+gameOverId = 2
+
+leaderboardId :: StateId 
+leaderboardId = 3
+
+victoryId :: StateId 
+victoryId = 4
+
+playId :: StateId 
+playId = 5
