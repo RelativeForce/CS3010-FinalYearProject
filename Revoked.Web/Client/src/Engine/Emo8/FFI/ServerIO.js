2321290e2d555c4ad5bb023dcaab084a8d9d8cc1
RelativeForce <joshiceddy@hotmail.com>
2019-12-24 11:09:51 +0000

    Fixed re-try error bug

--- Revoked.Web/Client/src/Engine/Emo8/FFI/ServerIO.js
+++ Revoked.Web/Client/src/Engine/Emo8/FFI/ServerIO.js
@@ -61,6 +61,28 @@ function send(requestData){
             else{
                 localRequest.result = true;
             }
+        },
+        error : function (jqXHR, exception) {
+            
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+
+            console.log(msg);
+            removeFromLocalStore(requestData);
         }
     });
 
