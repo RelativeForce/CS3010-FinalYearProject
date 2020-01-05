b9611c937eb7a77fb2c731b709fa77ec18e84d44
RelativeForce <joshiceddy@hotmail.com>
2020-01-05 12:42:06 +0000

    Added more tests

---
+++ Revoked.Web/Client/test/Test/Revoked/Data/Gun/Pistol/CanFire.purs
@@ -0,0 +1,73 @@
+module Test.Revoked.Data.Gun.Pistol.CanFire ( 
+    canFireTests 
+) where
+
+import Prelude
+
+import Constants (bulletSpeed)
+import Data.Gun.Pistol (Pistol, canFire, defaultPistol)
+import Test.Unit (TestSuite, suite, test)
+import Test.Unit.Assert (equal)
+
+canFireTests :: TestSuite
+canFireTests =
+    suite "Test.Revoked.Data.Gun.Pistol - canFire" do
+        test "canNotFireWhenOutOfAmmoAndNotInfiniteAmmo" do
+            let 
+                isInfinite = false
+                shotCount = 0
+                shotCoolDown = 0
+
+                expected = false 
+
+                result = canFire $ buildPistol isInfinite shotCount shotCoolDown
+            equal expected result
+        test "canNotFireWhenCoolingDownAndNotInfiniteAmmo" do
+            let 
+                isInfinite = false
+                shotCount = 1
+                shotCoolDown = 1
+
+                expected = false 
+
+                result = canFire $ buildPistol isInfinite shotCount shotCoolDown
+            equal expected result
+        test "canFireWhenHasAmmoAndNotCoolingDownAndNotInfiniteAmmo" do
+            let 
+                isInfinite = false
+                shotCount = 1
+                shotCoolDown = 0
+
+                expected = true 
+
+                result = canFire $ buildPistol isInfinite shotCount shotCoolDown
+            equal expected result
+        test "canNotFireWhenCoolingDownAndInfiniteAmmo" do
+            let 
+                isInfinite = true
+                shotCount = 1
+                shotCoolDown = 1
+
+                expected = false 
+
+                result = canFire $ buildPistol isInfinite shotCount shotCoolDown
+            equal expected result
+        test "canFireWhenHasNoAmmoAndNotCoolingDownAndInfiniteAmmo" do
+            let 
+                isInfinite = true
+                shotCount = 1
+                shotCoolDown = 0
+
+                expected = true 
+
+                result = canFire $ buildPistol isInfinite shotCount shotCoolDown
+            equal expected result
+
+buildPistol :: Boolean -> Int -> Int -> Pistol
+buildPistol isInfinite shotCount shotCoolDown = p
+    where 
+        basic = defaultPistol isInfinite { x: 0, y: 0 } 0
+        p = basic { 
+            shotCount = shotCount,
+            shotCoolDown = shotCoolDown 
+        }
--- Revoked.Web/Client/test/Test/Revoked/Data/Gun/Pistol/Pistol.purs
+++ Revoked.Web/Client/test/Test/Revoked/Data/Gun/Pistol/Pistol.purs
@@ -6,6 +6,7 @@ import Prelude
 
 import Effect (Effect)
 import Test.Revoked.Data.Gun.Pistol.BulletVelocity (bulletVelocityTests)
+import Test.Revoked.Data.Gun.Pistol.CanFire (canFireTests)
 import Test.Unit.Main (runTest)
 
 pistolTests :: Effect Unit
@@ -13,4 +14,5 @@ pistolTests = do
     -- Tests
     runTest do
         bulletVelocityTests
+        canFireTests
     -- Sub Modules
