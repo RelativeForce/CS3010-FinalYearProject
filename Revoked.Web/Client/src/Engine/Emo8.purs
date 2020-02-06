5c8f2176de9b5ef7766c79e2e5534aeb0a410acc
Joshua Eddy <joshiceddy@hotmail.com>
2020-02-06 17:26:24 +0000

    Removed last book remenants

--- Revoked.Web/Client/src/Engine/Emo8.purs
+++ Revoked.Web/Client/src/Engine/Emo8.purs
@@ -14,7 +14,6 @@ import Emo8.Input (mkInputSig)
 import Emo8.Interpreter.Draw (runDraw)
 import Emo8.Interpreter.Update (runUpdate)
 import Emo8.Types (Asset, MonitorSize)
-import Emo8.Utils (mkAsset)
 import Graphics.Canvas (CanvasElement, getCanvasElementById, getContext2D, setCanvasHeight, setCanvasWidth)
 import Signal (runSignal, sampleOn)
 import Signal.DOM (animationFrame)
@@ -25,14 +24,14 @@ emo8 :: forall s. Game s => s -> Asset -> MonitorSize -> Effect Unit
 emo8 state asset ms = withCanvas \canvas -> do
   setDim canvas ms
   context <- getContext2D canvas
-  bootAsset <- mkAsset []
-  let drawCtx = { ctx: context, mapData: asset.mapData, monitorSize: ms }
-      bootDrawCtx = { ctx: context, mapData: bootAsset.mapData, monitorSize: ms }
-
   frameSig <- animationFrame
   keyTouchInputSig <- poll
-  let keyTouchInputSampleSig = sampleOn frameSig keyTouchInputSig
-      inputSampleSig = mkInputSig keyTouchInputSampleSig
+
+  let
+    drawCtx = { ctx: context, mapData: asset.mapData, monitorSize: ms } 
+    keyTouchInputSampleSig = sampleOn frameSig keyTouchInputSig
+    inputSampleSig = mkInputSig keyTouchInputSampleSig
+
   stateSig <- foldEffect (\i -> runUpdate <<< (update asset i)) state inputSampleSig 
   runSignal $ map (\s -> (runDraw drawCtx <<< draw) s) stateSig
 
