479a21c894956a855b2d7f72350ac0042f9eb73e
Joshua Eddy <joshiceddy@hotmail.com>
2020-02-06 14:57:23 +0000

    Removed all emoji related stuff

--- Revoked.Web/Client/src/Engine/Emo8/Action/Draw.purs
+++ Revoked.Web/Client/src/Engine/Emo8/Action/Draw.purs
@@ -4,28 +4,18 @@ import Prelude
 
 import Control.Monad.Free (Free, liftF)
 import Emo8.Data.Color (Color)
-import Emo8.Data.Emoji (Emoji)
 import Emo8.Types (Deg, MapId, X, Y, Image, ScaledImage, Sprite, Size, TextHeight)
 
 type Draw = Free DrawF
 
-data DrawF n
-    = ClearScreen Color n
-    | DrawImageNoScaling Image X Y n
+data DrawF n = 
+    DrawImageNoScaling Image X Y n
     | DrawScaledImage ScaledImage X Y n
     | DrawRotatedScaledImage ScaledImage X Y Deg n
     | DrawSprite Sprite X Y n
     | DrawRotatedSprite Sprite X Y Deg n
     | DrawText String TextHeight X Y Color n
-    | Emo Appearance Emoji Size X Y n
-    | Emor Appearance Deg Emoji Size X Y n
     | DrawMap MapId Size X Y n
-
-data Appearance = Normal | Mirrored
-
--- | Clear screen with given color.
-cls :: Color -> Draw Unit
-cls c = liftF $ ClearScreen c unit
 
 drawImageNoScaling :: Image -> X -> Y -> Draw Unit
 drawImageNoScaling image x y = liftF $ DrawImageNoScaling image x y unit
@@ -45,22 +35,5 @@ drawRotatedSprite sprite x y angle = liftF $ DrawRotatedSprite sprite x y angle 
 drawText :: String -> TextHeight -> X -> Y -> Color -> Draw Unit
 drawText text height x y color = liftF $ DrawText text height x y color unit
 
--- | Draw emoji.
-emo :: Emoji -> Size -> X -> Y -> Draw Unit
-emo e size x y = liftF $ Emo Normal e size x y unit
-
--- | Draw rotated emoji.
-emo' :: Emoji -> Size -> X -> Y -> Draw Unit
-emo' e size x y = liftF $ Emo Mirrored e size x y unit
-
--- | Draw mirrored emoji.
-emor :: Deg -> Emoji -> Size -> X -> Y -> Draw Unit
-emor deg e size x y = liftF $ Emor Normal deg e size x y unit
-
--- | Draw mirrored rotated emoji.
-emor' :: Deg -> Emoji -> Size -> X -> Y -> Draw Unit
-emor' deg e size x y = liftF $ Emor Mirrored deg e size x y unit
-
--- | Draw tile map.
 drawMap :: MapId -> Size -> X -> Y -> Draw Unit
 drawMap mId size x y = liftF $ DrawMap mId size x y unit
