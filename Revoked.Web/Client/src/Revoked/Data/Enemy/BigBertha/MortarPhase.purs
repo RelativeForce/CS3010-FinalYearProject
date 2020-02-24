7767c1aabea7eedc9b22caae535709f608a7189a
RelativeForce <joshiceddy@hotmail.com>
2020-02-24 11:40:07 +0000

    Removed player position prediction

--- Revoked.Web/Client/src/Revoked/Data/Enemy/BigBertha/MortarPhase.purs
+++ Revoked.Web/Client/src/Revoked/Data/Enemy/BigBertha/MortarPhase.purs
@@ -5,7 +5,7 @@ import Prelude
 import Constants (gravity, mapTileSize, bigBerthaSpeed)
 import Data.Bullet (Bullet, newArcBullet)
 import Data.Player (Player(..))
-import Data.Int (toNumber, floor)
+import Data.Int (toNumber)
 import Emo8.Types (Position, Velocity, X)
 import Math (sqrt, abs)
 import Data.Enemy.BigBertha.Helper (playerInRange, updateVelocity, updatePosition, ensureLeftLimit, ensureRightLimit, coolDownShot)
@@ -36,30 +36,13 @@ horizontalVelocity target mortarPhase = (d * g) / ((2.0 * g * sqrt (2.0 * h)) - 
 verticalVelocity :: Number
 verticalVelocity = sqrt $ 2.0 * (abs gravity) * mortarApex
 
-predictPlayerPosition :: Player -> MortarPhase -> Position
-predictPlayerPosition (Player p) mortarPhase = pos
-    where
-        timeUnits = framesToIntercept p.pos mortarPhase
-        xOffset = if atLeftLimit mortarPhase then 0 else floor (timeUnits * p.velocity.xSpeed)
-        yOffset = if atLeftLimit mortarPhase then 0 else floor (timeUnits * p.velocity.ySpeed)
-        pos = {
-            x: p.pos.x + xOffset,
-            y: p.pos.y + yOffset
-        }
-
-framesToIntercept :: Position -> MortarPhase -> Number
-framesToIntercept target mortarPhase = (((mortarApex * 2.0) + l) * 2.0) / verticalVelocity
-    where
-        mortarPos = mortarPosition mortarPhase
-        l = toNumber $ mortarPhase.pos.y - target.y
-
 canFire :: Player -> MortarPhase -> Boolean
 canFire p mortarPhase = mortarPhase.shotCoolDown == 0 && playerInRange p mortarPhase.pos
 
 bulletVelocity :: Player -> MortarPhase -> Velocity
-bulletVelocity p mortarPhase = { xSpeed: xSpeed, ySpeed: ySpeed }
+bulletVelocity (Player p) mortarPhase = { xSpeed: xSpeed, ySpeed: ySpeed }
     where 
-        xSpeed = - horizontalVelocity (predictPlayerPosition p mortarPhase) mortarPhase
+        xSpeed = - horizontalVelocity p.pos mortarPhase
         ySpeed = verticalVelocity
 
 mortarPosition :: MortarPhase -> Position
