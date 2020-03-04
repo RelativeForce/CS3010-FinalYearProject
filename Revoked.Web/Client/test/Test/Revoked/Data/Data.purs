91c7534eebfba4eb469e23e21195cad07f0f0ae1
Joshua Eddy <joshiceddy@hotmail.com>
2020-03-04 12:16:00 +0000

    Added tests

--- Revoked.Web/Client/test/Test/Revoked/Data/Data.purs
+++ Revoked.Web/Client/test/Test/Revoked/Data/Data.purs
@@ -9,11 +9,13 @@ import Test.Revoked.Data.Enemy (enemyTests)
 import Test.Revoked.Data.Gun (gunTests)
 import Test.Revoked.Data.Bullet (bulletTests)
 import Test.Revoked.Data.Goal (goalTests)
+import Test.Revoked.Data.Helper (helperTests)
 import Test.Unit (TestSuite)
 
 dataTests :: TestSuite
 dataTests = do
     playerTests
+    helperTests
     enemyTests
     gunTests
     bulletTests
---
+++ Revoked.Web/Client/test/Test/Revoked/Data/Helper/IsDead.purs
@@ -0,0 +1,50 @@
+module Test.Revoked.Data.Helper.IsDead ( 
+    isDeadTests 
+) where
+
+import Prelude
+
+import Class.Object (class Object, class MortalEntity)
+import Data.Helper (isDead)
+import Test.Unit (TestSuite, suite, test)
+import Test.Unit.Assert (equal)
+
+isDeadTests :: TestSuite
+isDeadTests =
+    suite "Revoked.Data.Helper - isDead" do
+        test "SHOULD be alive WHEN health is above 0" do
+            let 
+                health = 1
+
+                expected = false 
+
+                result = isDead $ Test health
+            equal expected result
+        test "SHOULD be dead WHEN health is zero" do
+            let 
+                health = 0
+
+                expected = true 
+
+                result = isDead $ Test health
+            equal expected result
+        test "SHOULD be dead WHEN health is below zero" do
+            let 
+                health = -1
+
+                expected = true 
+
+                result = isDead $ Test health
+            equal expected result
+
+data FakeMortalEntity = Test Int
+
+instance objectEnemy :: Object FakeMortalEntity where
+    size (Test s) = { width: 0, height: 0 }
+    position (Test s) = { x: 0, y: 0 }
+    scroll offset (Test s) = Test $ s 
+
+instance mortalEntityPlayer :: MortalEntity FakeMortalEntity where
+    health (Test m) = m
+    damage (Test m) healthLoss = Test $ m - healthLoss 
+    heal (Test m) healthBonus = Test $ m + healthBonus 
