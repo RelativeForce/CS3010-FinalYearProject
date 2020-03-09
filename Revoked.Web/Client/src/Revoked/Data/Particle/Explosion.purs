d64ef28ba6b1f2b5c6f39068a1c88d33b8f81d36
RelativeForce <joshiceddy@hotmail.com>
2020-03-09 17:15:11 +0000

    Documented Particle

--- Revoked.Web/Client/src/Revoked/Data/Particle/Explosion.purs
+++ Revoked.Web/Client/src/Revoked/Data/Particle/Explosion.purs
@@ -2,31 +2,26 @@ module Revoked.Data.Particle.Explosion where
 
 import Prelude
 
-import Emo8.Types (Position, Sprite, X, Size)
+import Emo8.Types (Position, Sprite)
 import Emo8.Data.Sprite (incrementFrame, isLastFrame)
 
 import Revoked.Assets.Sprites as S
 
+-- | Represents the state of an explosion
 type Explosion = { 
     pos :: Position,
     sprite :: Sprite
 }
 
-explosionSize :: Explosion -> Size
-explosionSize e = e.sprite.size
-
-explosionPosition :: Explosion -> Position
-explosionPosition e = e.pos
-
-scrollExplosion :: X -> Explosion -> Explosion
-scrollExplosion offset e = e { pos = { x: e.pos.x + offset, y: e.pos.y }}
-
+-- | Progresses an Explosion through its sprite animation until the end. Once there the 
+-- | Explosion is moved out of the screen bounds such that it is cleaned up.
 updateExplosion :: Explosion -> Explosion
 updateExplosion e = e { pos = { x: e.pos.x , y: e.pos.y + deltaY }, sprite = incrementFrame e.sprite }
     where
         moveOffScreen = isLastFrame e.sprite
         deltaY = if moveOffScreen then -5000 else 0
 
+-- | Builds a explosion with a given `Position`
 defaultDroneExplosion :: Position -> Explosion
 defaultDroneExplosion p = {
     pos: p,
--- Revoked.Web/Client/src/Revoked/Data/Particle/Particle.purs
+++ Revoked.Web/Client/src/Revoked/Data/Particle/Particle.purs
@@ -6,31 +6,35 @@ import Emo8.Class.Object (class ObjectDraw, class Object, position)
 import Emo8.Action.Draw (drawSprite)
 import Emo8.Types (Position)
 
-import Revoked.Data.Particle.Ghost (Ghost, scrollGhost, updateGhost, defaultMarineGhost, ghostSize, ghostPosition)
-import Revoked.Data.Particle.Explosion (Explosion, scrollExplosion, updateExplosion, defaultDroneExplosion, explosionSize, explosionPosition)
+import Revoked.Data.Particle.Ghost (Ghost, updateGhost, defaultMarineGhost)
+import Revoked.Data.Particle.Explosion (Explosion, updateExplosion, defaultDroneExplosion)
 
+-- | Wraps the states of the different types of Particle
 data Particle = 
     GhostParticle Ghost |
     ExplosionParticle Explosion
 
 instance objectParticle :: Object Particle where
-    size (GhostParticle g) = ghostSize g
-    size (ExplosionParticle e) = explosionSize e
-    position (GhostParticle g) = ghostPosition g
-    position (ExplosionParticle e) = explosionPosition e
-    scroll offset (GhostParticle g) = GhostParticle $ scrollGhost offset g
-    scroll offset (ExplosionParticle e) = ExplosionParticle $ scrollExplosion offset e
+    size (GhostParticle g) = g.sprite.size
+    size (ExplosionParticle e) = e.sprite.size
+    position (GhostParticle g) = g.pos
+    position (ExplosionParticle e) = e.pos
+    scroll offset (GhostParticle g) = GhostParticle $ g { pos = { x: g.pos.x + offset, y: g.pos.y }}
+    scroll offset (ExplosionParticle e) = ExplosionParticle $ e { pos = { x: e.pos.x + offset, y: e.pos.y }}
 
 instance objectDrawParticle :: ObjectDraw Particle where
     draw o@(GhostParticle g) = drawSprite g.sprite (position o).x (position o).y
     draw o@(ExplosionParticle e) = drawSprite e.sprite (position o).x (position o).y
 
+-- | Updates a `Particle` based on that particle types's defined update logic.
 updateParticle :: Particle -> Particle
 updateParticle (GhostParticle g) = GhostParticle $ updateGhost g
 updateParticle (ExplosionParticle e) = ExplosionParticle $ updateExplosion e
 
+-- | Builds a Ghost with a given `Position`
 defaultGhostParticle :: Position -> Particle
 defaultGhostParticle = defaultMarineGhost >>> GhostParticle
 
+-- | Builds a Explosion with a given `Position`
 defaultExplosionParticle :: Position -> Particle
 defaultExplosionParticle = defaultDroneExplosion >>> ExplosionParticle
