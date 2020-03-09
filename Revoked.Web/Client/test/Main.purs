cb6f3c0ee166ed032602b6eec42ab06525916bad
RelativeForce <joshiceddy@hotmail.com>
2020-03-09 13:34:30 +0000

    Follow up

--- Revoked.Web/Client/test/Main.purs
+++ Revoked.Web/Client/test/Main.purs
@@ -5,12 +5,12 @@ import Prelude
 import Effect (Effect)
 import Test.Unit.Main (runTest)
 
-import Test.Engine (engineTests)
+import Test.Engine.Emo8 (emo8Tests)
 import Test.Revoked (revokedTests)
 
 main :: Effect Unit
 main = do
   runTest do
-    engineTests
+    emo8Tests
     revokedTests
 
